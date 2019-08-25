a0aa6126aba98e79ac3ee3ab475f22f714987ae1
Vinicius Marino <vmarino@Viniciuss-MacBook-Pro.local>
2019-08-24 22:55:40 -0300

    added select folder class

---
+++ select/racer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+
+}
+
+func ping(url string) chan bool {
+
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
+
+}
+
+func measureResponseTime(url string) time.Duration {
+
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+
+}
